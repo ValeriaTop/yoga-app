b7763968871456cb63cbc5a618cfa8c1eb7b82eb
Valeria Top <gvtorresp27@gmail.com>
2024-12-10 12:15:35 -0600

    create clases-yoga.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index-yoga.html', // Archivo HTML principal
-    './login-yoga.html', // Archivo HTML principal
+    './index-yoga.html', 
+    './login-yoga.html', 
+    './clases-yoga.html',
     './assets/**/*.{js,ts,jsx,tsx,vue}', // Archivos Vue, JS, etc.
   ],
   theme: {
